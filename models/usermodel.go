bd877615fde390963b3d16af65abeaadc45f97af
rezhasastrapc <rezha.sastra@gmail.com>
2026-08-11 15:19:32 +0700

    Update import paths for go-auth package

--- models/usermodel.go
+++ models/usermodel.go
@@ -3,8 +3,8 @@ package models
 import (
 	"database/sql"
 
-	"github.com/jeypc/go-auth/config"
-	"github.com/jeypc/go-auth/entities"
+	"github.com/rezhasastrapc/go-auth/config"
+	"github.com/rezhasastrapc/go-auth/entities"
 )
 
 type UserModel struct {
